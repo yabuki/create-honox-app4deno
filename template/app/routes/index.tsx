54fb6baaf737aeef48d35e5c0f8c218dd3c7b198
Yukiharu YABUKI <yabuki@netfort.gr.jp>
2025-04-22 05:44:35 +0900

    Apply 'deno fmt style' to template files

--- template/app/routes/index.tsx
+++ template/app/routes/index.tsx
@@ -1,13 +1,13 @@
-import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
+import { createRoute } from "honox/factory";
+import Counter from "../islands/counter";
 
 export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'no name'
+  const name = c.req.query("name") ?? "no name";
   return c.render(
     <div>
       <title>{name}</title>
       <h1>Hello, {name}!</h1>
       <Counter />
-    </div>
-  )
-})
+    </div>,
+  );
+});
