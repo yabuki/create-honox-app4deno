61d1a7746321cb2504350b00c5262cb4eac7a22a
Yukiharu YABUKI <yabuki@netfort.gr.jp>
2025-04-24 13:54:55 +0900

    Drop tests

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -1,9 +1,9 @@
 // import { assertEquals } from "@std/assert";
 
-import { createNewDirectory } from "../src/files.ts";
+// import { createNewDirectory } from "../src/files.ts";
 
 
-Deno.test(async function createDir() {
-  const path_and_dirname = "/tmp/hoge/";
-  await createNewDirectory(path_and_dirname);
-});
+// Deno.test(async function createDir() {
+//   const path_and_dirname = "/tmp/hoge/";
+//   await createNewDirectory(path_and_dirname);
+// });
