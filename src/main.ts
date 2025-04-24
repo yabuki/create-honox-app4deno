37849851cb711133295060cc66fed157f393faa4
Yukiharu YABUKI <yabuki@netfort.gr.jp>
2025-04-25 03:35:21 +0900

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,19 @@ type options = {
 	force: boolean;
 };
 
+type projectType = {
+	dirName: string | undefined;
+	baseDir: string;
+	dirNameWithPath: string;
+	overWrite: boolean;
+};
+
+type templateType = {
+	baseDir: string;
+	fileName: string;
+	fullPath: string;
+};
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 // プログラムのエントリーポイントかどうかを確認する。
 if (import.meta.main) {
@@ -53,93 +66,114 @@ if (import.meta.main) {
 				options: { directory: string | true; force: boolean },
 				projectDirName?: string,
 			): Promise<void> => {
-				let projctDirNameWithPath: string;
-				// options.directory が オプション(-d / --directory)で与えられた
-				// プロジェクトディレクトリ名 または カレントディレクトリの"." 
-				// になる。
-				const baseDir: string = typeof options.directory === "string"
-					? options.directory
-					: ".";
+				// HonoXのプロジェクトを生成するのに必要な情報を持つオブジェクト
+				const project = {
+					dirName: projectDirName, // ユーザが与えたプロジェクト名 与えられないこともある。
+					// baseDir: オプションで与えられるかも知れないパス
+					// options.directory が オプション(-d / --directory)で与えられた
+					// プロジェクトディレクトリ名 または カレントディレクトリの"."
+					// になる。
+					baseDir: typeof options.directory === "string"
+						? options.directory
+						: ".",
+					dirNameWithPath: "", // プロジェクトディレクトリ名とパスを結合したもの
+					overWrite: options.force, // 上書き指定されたか? trueなら上書きする。
+				};
+				// templatを展開するのに必要な情報を持つオブジェクト
+				const template = {
+					baseDir: Deno.makeTempDirSync({
+						prefix: "create-honox-app4deno",
+					}),
+					fileName: "template.tar.gz",
+					fullPath: "",
+					setFullPath: function (template: templateType): string {
+						return resolve(template.baseDir, template.fileName);
+					},
+				};
+				// Deno がインストールされているか?
 				if (!checkDenoInstalled()) {
 					$.logError("Deno is not installed.");
 					$.logError("Please install Deno.");
 					Deno.exit(1);
 				}
-				if (projectDirName) {
-					// プロジェクトディレクトリ作成処理 (projectName, options.directory を利用)
-					projctDirNameWithPath = resolve(
-						baseDir,
-						projectDirName,
-					);
-					$.logLight("Project Dirname with Path: ", projctDirNameWithPath);
-				} else {
-					// 引数がない場合は、プロジェクト名を聞く。
-					$.logWarn("You did not give project directory name.");
-					projctDirNameWithPath = resolve(
-						baseDir,
-						await setProjectName(),
-					);
-				}
-				// $.log("force: ", options.force);
-				// 存在チェック
-				if (isDircectoryExists(projctDirNameWithPath)) {
-					if (options.force) {
-						$.logWarn("Force option is set. Overwriting...");
-						// ここで上書き処理を行う
-						if (await askOverwrite()) {
-							$.logWarn("Overwriting...");
-							await removeDirectory(projctDirNameWithPath);
-						} else {
-							$.logWarn("Aborting...");
-							Deno.exit(1);
-						}
-					} else {
-						$.log("Directory already exists. Use --force to overwrite.");
-						Deno.exit(1);
-					}
-				}
+				await decideProjectPath(project);
+				await confirmOverwrite(project);
 				//
-				const templatePath = Deno.makeTempDirSync({ prefix: "create-honox-app4deno" });
-				const templatePath_and_File = resolve(
-					templatePath,
-					"template.tar.gz",
-				);
-				console.log("Writing template to", templatePath_and_File);
+				template.fullPath = template.setFullPath(template);
 
-				// テンポラリディレクトリに template.tar.gz を展開する。
-				writeEmbbedfile(templatePath_and_File, embeddedFile);
-
-				// // Read a file
-				// const templateContent: Uint8Array = Deno.readFileSync(
-				// 	templatePath_and_File,
-				// );
-
-				// // バイナリ内容をファイルに書き出す
-				// Deno.writeFileSync(templatePath_and_File, templateContent);
-
-				console.log("Unpacking template to", templatePath_and_File);
-
-				for await (
-					const entry
-						of (await Deno.open(templatePath_and_File, { read: true }))
-							.readable
-							.pipeThrough(new DecompressionStream("gzip"))
-							.pipeThrough(new UntarStream())
-				) {
-					// tempdirで作ったディレクトリに展開する。
-					const path = resolve(templatePath, normalize(entry.path));
-					await Deno.mkdir(dirname(path), { recursive: true });
-					await entry.readable?.pipeTo((await Deno.create(path)).writable);
-				}
+				extractTarGz(template);
 				//
 				await createNewDirectory(
-					resolve(templatePath, "template/"),
-					projctDirNameWithPath,
+					resolve(template.baseDir, "template/"),
+					project.dirNameWithPath,
 				);
-				await installNpmModules(projctDirNameWithPath);
+				await installNpmModules(project.dirNameWithPath);
 				// mkTempDirの後始末
-				await removeDirectory(templatePath);
+				await removeDirectory(template.baseDir);
 			},
 		)
 		.parse(Deno.args);
+}
+
+/**
+ * @param project {projectType}
+ * @returns {Promise<void>}
+ * @description プロジェクトのディレクトリ名を決定する。
+ */
+async function decideProjectPath(project: projectType): Promise<void> {
+	if (project.dirName) {
+		project.dirNameWithPath = resolve(
+			project.baseDir,
+			project.dirName,
+		);
+		$.logLight("Project Dirname with Path: ", project.dirNameWithPath);
+	} else {
+		// 引数がない場合は、プロジェクト名を聞く。
+		$.logWarn("You did not give project directory name.");
+		project.dirNameWithPath = resolve(
+			project.baseDir,
+			await setProjectName(),
+		);
+	}
+}
+
+async function confirmOverwrite(project: projectType): Promise<void> {
+	if (isDircectoryExists(project.dirNameWithPath)) {
+		if (project.overWrite) {
+			$.logWarn("Force option is set. Overwriting...");
+			// ここで上書き処理を行う
+			if (await askOverwrite()) {
+				$.logWarn("Overwriting...");
+				await removeDirectory(project.dirNameWithPath);
+			} else {
+				$.logWarn("Aborting...");
+				Deno.exit(1);
+			}
+		} else {
+			$.log("Directory already exists. Use --force to overwrite.");
+			Deno.exit(1);
+		}
+	}
+}
+
+async function extractTarGz(
+	template: templateType,
+): Promise<void> {
+	// テンポラリディレクトリに template.tar.gz を展開する。
+	console.log("Writing template.tar.gz to", template.fullPath);
+	writeEmbbedfile(template.fullPath, embeddedFile);
+
+	console.log("Unpacking template to", template.fullPath);
+
+	for await (
+		const entry of (await Deno.open(template.fullPath, { read: true }))
+			.readable
+			.pipeThrough(new DecompressionStream("gzip"))
+			.pipeThrough(new UntarStream())
+	) {
+		// tempdirで作ったディレクトリに展開する。
+		const path = resolve(template.baseDir, normalize(entry.path));
+		await Deno.mkdir(dirname(path), { recursive: true });
+		await entry.readable?.pipeTo((await Deno.create(path)).writable);
+	}
 }
